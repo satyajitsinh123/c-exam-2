574c002827f7189f8157fd420ef0c059ec31511a
satyajitsinh123 <satyajitsinh63@gamil.com>
2024-01-30 12:51:06 +0530

    hello

---
+++ index.c
@@ -0,0 +1,37 @@
+// #include<stdio.h>
+// int fun(int s){
+//     if(s>10){
+//         return 0;
+//     }
+//     printf("%d ",s);
+//     fun(s+1);
+// }
+// int main(){
+
+// fun(1);
+
+
+
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+// #include <string.h>
+// int main()
+// {
+//     char color[]="red";
+//     printf("%s",strupr(color));
+
+//     return 0;
+// }
+
+
+#include <stdio.h>
+
+int main()
+{
+    
+
+    return 0;
+}
